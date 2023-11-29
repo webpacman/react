8f85b8b93882fad95e6ce67bbc591c577587d367
vshevchuk <shevchukvitalik@bk.ru>
2023-11-29 12:08:48 +0300

    Build settings and deploy on dev.vshevchuk.ru

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,43 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: './src/index.tsx',
-  output: {
-    filename: '[name].[contenthash].bundle.js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
-  },
-  devServer: {
-    static: './build',
-    hot: true,
-    port: 3000,
-    open: {
-      app: {
-        name: 'Google Chrome',
+//console.debug(process.env.NODE_ENV);
+
+module.exports = (env) => {
+  return {
+    mode: env.production ? 'production' : 'development',
+    devtool: env.production ? 'source-map' : 'inline-source-map',
+    entry: './src/index.tsx',
+    output: {
+      filename: '[name].[contenthash].bundle.js',
+      path: path.resolve(__dirname, 'build'),
+      clean: true,
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+    devServer: {
+      static: './build',
+      hot: true,
+      port: 3000,
+      open: {
+        app: {
+          name: 'Google Chrome',
+        },
       },
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      }
-    ]
-  },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'Сайт визитка',
-    template: './public/index.html',
-  })],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        }
+      ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+      title: 'Сайт визитка',
+      template: './public/index.html',
+    })],
+  }
 };
