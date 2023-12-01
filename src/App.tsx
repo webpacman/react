d7bbf81b131d117e4d15f237c1c2a737908d93f2
vshevchuk <shevchukvitalik@bk.ru>
2023-12-01 19:07:14 +0300

    Fix lint error

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import styles from './App.module.scss'
 
-export const App = () => {
+export const App = (): React.JSX.Element => {
   const text = 'App component'
   return <div className={styles.App}>{text}</div>
 }
