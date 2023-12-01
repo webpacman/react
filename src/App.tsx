53b4b30bed765eba85df5fcbf1f2c3246e89fd32
vshevchuk <shevchukvitalik@bk.ru>
2023-12-01 19:06:12 +0300

    Check lint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from 'react'
 
-import styles from './App.module.scss';
+import styles from './App.module.scss'
 
-export const App = (): React.JSX.Element => {
-  const text = 'App component';
-  return <div className={styles.App}>{text}</div>;
-};
+export const App = () => {
+  const text = 'App component'
+  return <div className={styles.App}>{text}</div>
+}
