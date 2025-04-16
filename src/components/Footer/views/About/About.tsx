b03dec09da72cdf038fce028ab510bf2013842e7
vshevchuk <shevchukvitalik@bk.ru>
2025-04-16 23:04:06 +0300

    feat(footer): реализация футера

--- src/components/Footer/views/About/About.tsx
+++ src/components/Footer/views/About/About.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
 
+import styles from "./About.module.scss";
+
 export const About: FC = () => {
   return (
-    <p className="footer-about">
-      &copy;&nbsp;Шевчук&nbsp;Виталий 1995&nbsp;-&nbsp;2023
+    <p className={styles.about}>
+      &copy;&nbsp;Шевчук&nbsp;Виталий 1995&nbsp;-&nbsp;
+      {new Date().getFullYear()}
     </p>
   );
 };
--- src/components/Footer/views/Footer/Footer.tsx
+++ src/components/Footer/views/Footer/Footer.tsx
@@ -1,14 +1,21 @@
 import { FC, PropsWithChildren } from "react";
 
+import { Flex, FlexAlign, FlexDirection } from "@/common/Flex";
+
 import { About } from "../About/About";
 import { Socials } from "../Socials/Socials";
-
+import styles from "./Footer.module.scss";
 export const Footer: FC<PropsWithChildren> = () => {
   return (
-    <footer className="footer section-grey">
+    <Flex
+      tag="footer"
+      direction={FlexDirection.COLUMN}
+      align={FlexAlign.CENTER}
+      className={styles.footer}
+    >
       <Socials />
 
       <About />
-    </footer>
+    </Flex>
   );
 };
